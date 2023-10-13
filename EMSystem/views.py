522a37689b850e3b278507837eb8cdad87ad38fa
naomuhibu <sanchequai@hotmail.com>
2023-10-14 10:07:17 +1300

    fixed  error

--- EMSystem/views.py
+++ EMSystem/views.py
@@ -5,10 +5,8 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-
-@api_view('GET','POST')
+@api_view(['GET', 'POST'])
 def Seismic_list(request):
-
     if request.method == 'GET':
         EMSystem = SeismicIntensity.objects.all()
         serializer = SeismicSerializer(EMSystem, many=True)
@@ -20,19 +18,24 @@ def Seismic_list(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-@api_view('GET', 'PUT' , 'DELATE')
-def Seismic_Detail(request, id):
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def Seismic_Detail(request, id):
     try:
-        Seisimic = SeismicIntensity.objects.get(pk=id)
+        place_name = SeismicIntensity.objects.get(pk=id)
     except SeismicIntensity.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        pass
-    elif request.method == 'POST':
-        pass
-    elif request.method == 'DELATE':
-        pass
-
+        serializer = SeismicSerializer(place_name)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SeismicSerializer(place_name, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        place_name.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)

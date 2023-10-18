90e27cdd8de4d8d42c304d6a8cbdb8eb4e6366f4
Thierry <270067227@yoobeestudent.ac.nz>
2023-10-18 15:59:17 +1300

    feat(request.py) fetch raw data from api

---
+++ request.py
@@ -0,0 +1,47 @@
+# IMPORTS
+import requests
+import re
+
+# Fetch raw data
+def fetchRawEarthquakeData():
+    # Fetch raw data from the API
+    print("Fetching earthquake data...")
+    try:
+        response = requests.get("https://api.geonet.org.nz/intensity?type=measured")
+        if response.status_code == 200:
+            data = response.json()
+            return data
+        else:
+            print(f"Failed to get data: {response.status_code}")
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
+        return None
+
+def extractData():
+    # Extract the data into a list of coordinates with magnitudes
+    data = fetchRawEarthquakeData()
+
+    if data is not None:
+        print("Successfully get data")
+
+        # Parsing the JSON data
+        if "features" in data:
+            for feature in data["features"]:
+                if "geometry" in feature and "properties" in feature:
+                    coordinates = feature["geometry"]["coordinates"]
+                    mmi = feature["properties"]["mmi"]
+                    print(f"Coordinates: {coordinates}, MMI: {mmi}")
+    return
+
+def calculateDistance(coords1, coords2):
+    # Calculate the distance between two coordinates
+    return
+
+def getNearestLocation(coords):
+    # Find the nearest location to a given coordinate
+    return
+
+def convertToActiveLocations():
+    # Convert the data into a list of active locations
+    return

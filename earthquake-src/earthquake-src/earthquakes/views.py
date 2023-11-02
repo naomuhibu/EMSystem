0abc506e0b516daa6d1c9a5ecb0dfd72af57707e
naomuhibu <sanchequai@hotmail.com>
2023-11-02 17:33:16 +1300

    push command error

--- earthquake-src/earthquake-src/earthquakes/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-
-from .models import Earthquake
-from .serializers import EarthquakeSerailizer
-
-class EarthquakeViewSet(viewsets.ModelViewSet):
-    queryset = Earthquake.objects.all()
-    serializer_class = EarthquakeSerailizer

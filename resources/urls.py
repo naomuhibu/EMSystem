fbed0c795309b878d3a32b5befbe513f1713b24d
naomuhibu <sanchequai@hotmail.com>
2023-11-13 12:09:59 +1300

    demo of leaflet map

--- resources/urls.py
+++ resources/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EarthquakeAPI
+from .views import EarthquakeAPI, IndexView
 
 urlpatterns = [
     path('Earthquake/api', EarthquakeAPI.as_view(), name='Earthquake_API'),
+    path('',IndexView .as_view(),name='index')
 ]
--- resources/views.py
+++ resources/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from django.core.serializers import serialize
+from django.views.generic import TemplateView
+from django.http import JsonResponse
+from rest_framework import generics
 from .models import Earthquake
 from .serializers import EarthquakeSerializer
-from rest_framework import generics
-# Create your views here.
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def earthquakes(self):
+        earthquakes = Earthquake.objects.all()
+        serializer = EarthquakeSerializer(earthquakes, many=True)
+        geojson_data = serializer.data
+        return JsonResponse({'earthquakes': geojson_data}, content_type='application/json', safe=False)
+    
 class EarthquakeAPI(generics.ListAPIView):
     queryset = Earthquake.objects.all()
     serializer_class = EarthquakeSerializer
